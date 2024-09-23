62029bff094d56bfbffe0bc14da781ecced74b73
kalscium <kalscium@protonmail.com>
2024-09-23 17:16:25 +1000

    export: markdown: added proper and nice looking exportation for markdown

--- src/export.rs
+++ src/export.rs
@@ -1,5 +1,5 @@
 use std::{fs::{self, File}, io::Write};
-
+use chrono::{DateTime, Datelike, Local, Utc};
 use log::info;
 
 use crate::{database::Database, get_dir, thought::Thought};
@@ -20,8 +20,9 @@ fn export_markdown(path: &str) {
     let database = Database::load(get_dir()).expect("database either corrupt or non-existent");
     let mut file = File::create(path).unwrap();
 
-    // write the title
+    // write the title & initialise the 'last time' variable
     file.write_all("# Thoughts :D\n---\n".as_bytes()).unwrap();
+    let mut last: Option<DateTime<Utc>> = None;
 
     // write the entries
     for bytes in database {
@@ -29,9 +30,50 @@ fn export_markdown(path: &str) {
         let thought = bincode::deserialize(&bytes).expect("thought database is corrupt");
 
         // extract the thought and time
-        let Thought(thought, _) = thought;
+        let Thought(thought, time) = thought;
+
+        // if there is a timestamp then check it
+        // diff day, print date
+        // if it's been longer than an 30 mins, print time
+        if let Some(time) = time {
+            // get the last time or otherwise use a generic time
+            let last = last.unwrap_or(DateTime::from_timestamp_nanos(0));
+            
+            // check the day or month or year
+            if last.day() != time.day() || last.month() != time.month() || last.year() != time.year() {
+                let time: DateTime<Local> = DateTime::from(time);
+
+                // format the date
+                let format = &format!(
+                    "%A, %-d{} of %B %Y, `%I:%M %p`",
+                    // get the suffix (may replace later with better alternative)
+                    match time.day() {
+                        t if t % 10 == 1 && t % 100 != 11 => "st",
+                        t if t % 10 == 2 && t % 100 != 12 => "nd",
+                        t if t % 10 == 3 && t % 100 != 13 => "rd",
+                        _ => "th",
+                    }
+                );
+                let date = format!("## {}\n", time.format(format));
+
+                // write the formatted date
+                file.write_all(date.as_bytes()).unwrap();
+            } else if (time.time() - last.time()).num_minutes() > 30 { // check if it's within 30 minutes
+                // format the time and write it
+                let time: DateTime<Local> = DateTime::from(time);
+                let time = time.format("`%I:%M %p`");
+                let time = format!("#### {time}\n");
+
+                // write the formatted time
+                file.write_all(time.as_bytes()).unwrap();
+            }
+        }
+
+        // update last
+        last = time;
 
         // write the thought to the file
+        let thought = format!("- {thought}\n");
         file.write_all(thought.as_bytes()).unwrap();
     }
 
